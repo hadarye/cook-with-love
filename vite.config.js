d852d9c1ea9faa2218a284158258915c1980fae6
hadarye <hadar.yeheskiel@gmail.com>
2023-10-31 19:11:00 +0200

    :(

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cook-with-love/"
+  base: "https://hadarye.github.io/cook-with-love/"
 })
