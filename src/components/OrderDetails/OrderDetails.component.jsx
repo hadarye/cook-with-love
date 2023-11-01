2d7684e6c8efaa2225608a58ae0a869b2b1cc9ea
hadarye <hadar.yeheskiel@gmail.com>
2023-11-01 20:02:14 +0200

    css done

--- src/components/OrderDetails/OrderDetails.component.jsx
+++ src/components/OrderDetails/OrderDetails.component.jsx
@@ -7,6 +7,7 @@ const OrderDetails = (props) => {
     const [dishesArr, setDishesArr] = useState(props.dishes);
     const [isShowPopUp, setIsShowPopUp] = useState(false);
     const [chosenDish, setChosenDish] = useState({});
+
     let date = new Date(props.date);
     const dateRef = useRef(` ${String(date.getDate()).padStart(2, "0")}/${String(date.getMonth() + 1).padStart(2, "0")}`);
     const timeRef = useRef(`${date.getHours()}:${String(date.getMinutes()).padStart(2, "0")}`);
@@ -24,15 +25,22 @@ const OrderDetails = (props) => {
         <div className='order-details'>
             <div onClick={() => props.closeDescription()} className='back-arrow'></div>
             <h2 className='order-details-title'>{props.orderType}</h2>
-            <div className='order-container'>
-                {dishesArr.map((dish) => (
-                    <div key={dish.type} className='dish-details'>
-                        <p>{dish.type}</p>
-                        <p>{dish.count}</p>
-                        <button onClick={() => ShowPopUp(dish)} className='register-btn'>הרשמה</button>
-                    </div>
-                ))}
+            <div className='details-container'>
+                <div className='details-top-bar'>
+                    <div>מנה</div>
+                    <div>כמות</div>
+                </div>
+                <div className='order-container'>
+                    {dishesArr.map((dish) => (
+                        <div key={dish.type} className='dish-details'>
+                            <p>{dish.type}</p>
+                            <p>{dish.count}</p>
+                            <button onClick={() => ShowPopUp(dish)} className='register-btn'>הרשמה</button>
+                        </div>
+                    ))}
+                </div>
             </div>
+
             <p>כתובת: {props.adress}</p>
 
             <div className={isShowPopUp ? " " : 'hidden'}>
--- src/components/RegisterPopUp/RegisterPopUp.component.jsx
+++ src/components/RegisterPopUp/RegisterPopUp.component.jsx
@@ -1,9 +1,11 @@
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react';
+import add from '../../assets/images/add.png';
+import subtract from '../../assets/images/subtraction.png'
 import './RegisterPopUp.styles.css'
 
 const RegisterPopUp = (props) => {
-
+    const [numInputValue, setNumInputValue] = useState(1);
     return (
         <>
             <div className='black-screen'>
@@ -21,10 +23,18 @@ const RegisterPopUp = (props) => {
                         </div>
                         <div className='input-container'>
                             מה תכיני?
-                            <input className='form-input' placeholder='פרטי על המנה'></input>
+                            <textarea resize="none" rows="3" cols="50" className='form-textarea' placeholder='פרטי על המנה'></textarea>
                         </div>
-                        <input className='number-input' type="number" step="1" defaultValue="1" min="1" max="100" />
-                        <button className='register-btn'>הרשמה</button>
+                        <div className='number-container'>
+                            מספר המנות שתכיני:
+                            <div className='number-controls'>
+                                <img src={add} className='number-controller' onClick={() => setNumInputValue(numInputValue + 1)}/>
+                                <input className='number-input' type="number" step="1" value={numInputValue} min="1" max="100" />
+                                <img  onClick={() => setNumInputValue(numInputValue - 1)} src={subtract} className='number-controller'/>
+                            </div>
+                        </div>
+
+                        <button className='register-btn popup-btn'>הרשמה</button>
                     </form>
                 </div>
             </div>
