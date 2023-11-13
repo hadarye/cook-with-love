6ad96d02ebbff97fd2a2dc4bc053d004ddac523c
hadarye <hadar.yeheskiel@gmail.com>
2023-11-13 12:10:10 +0200

    new order form works

--- src/components/NewOrderForm/NewOrderForm.component.jsx
+++ src/components/NewOrderForm/NewOrderForm.component.jsx
@@ -13,33 +13,45 @@ const NewOrderForm = (props) => {
     });
     const manager_name_ref = useRef("");
     const order_name_ref = useRef("");
-    const [orderType, setOrderType] = useState("");
+    const order_type_ref = useRef("");
     const contact_name_ref = useRef("");
+    const collecting_date_ref = useRef("");
+    // const collecting_date_input_ref = useRef("");
+    const collecting_person_name_ref = useRef("");
+    const collecting_location_ref = useRef("");
+    const collecting_person_phone_ref = useRef("");
     const contact_phone_ref = useRef("");
-    const isKosherRef = useRef(false);
+    const total_ref = useRef("");
+    const total_kosher_ref = useRef("");
+    const total_vegetarians_ref = useRef("");
 
     const formSubmit = async (e) => {
-        e.preventDefault();
-        const url = 'https://8nkv5zptli.execute-api.eu-west-1.amazonaws.com/dev/dishes/register';
-        const objRegistration = {
-            "order_id": String(props.order_Id),
-            "baker_name": String(nameRef.current.value),
-            "dish_type": String(props.dishType),
-            "baker_phone": String(phoneRef.current.value),
-            "baker_dish_type": String(typeRef.current.value),
-            "baker_kosher": Boolean(isKosherRef.current),
-            "number_of_dishes": Number(numInputValue)
+        // e.preventDefault();
+        const url = 'https://8nkv5zptli.execute-api.eu-west-1.amazonaws.com/dev/orders/create';
+        const objNewOrder = {
+            "manager_name": String(manager_name_ref.current.value),
+            "order_name": String(order_name_ref.current.value),
+            "order_type": String(order_type_ref.current),
+            "contact_name": String(contact_name_ref.current.value),
+            "contact_phone": String(contact_phone_ref.current.value),
+            "collecting_date": String(collecting_date_ref.current.value),
+            "collecting_person_name": String(collecting_person_name_ref.current.value),
+            "collecting_location": String(collecting_location_ref.current.value),
+            "collecting_person_phone": String(collecting_person_phone_ref.current.value),
+            "total": Number(total_ref.current.value),
+            "total_kosher": Number(total_kosher_ref.current.value),
+            "total_vegetarians": Number(total_vegetarians_ref.current.value)
         }
-        console.log('registrasion ', objRegistration)
+        console.log('registrasion ', objNewOrder);
         const response = await fetch(url, {
             "method": "POST",
-            body: JSON.stringify(objRegistration),
+            body: JSON.stringify(objNewOrder),
         });
         console.log('post completed');
-        clearForm();
+        // clearForm();
         const result = await response.json()
         console.log(result);
-        console.warn('refresh')
+        console.warn('refresh');
     }
 
     const clearForm = () => {
@@ -69,7 +81,7 @@ const NewOrderForm = (props) => {
                         סוג ההזמנה:
                         {Object.keys(orderNamesRef.current).map((key, index) => (
                             <div key={key}>
-                                <input type='radio' name='order_type' id={key} value={key} onClick={() => setOrderType(key)}></input>
+                                <input type='radio' name='order_type' id={key} value={key} onClick={() => order_type_ref.current = key}></input>
                                 <label htmlFor={key}>{orderNamesRef.current[key]}</label>
                             </div>
 
@@ -81,35 +93,35 @@ const NewOrderForm = (props) => {
                     </div>
                     <div className='order-inputs-container'>
                         טלפון ליצירת קשר:
-                        <input type='text' name='contact_phone' ref={contact_phone_ref} pattern='/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im' placeholder='טלפון ליצירת קשר'></input>
+                        <input type='text' name='contact_phone' ref={contact_phone_ref} placeholder='טלפון ליצירת קשר'></input>
                     </div>
                     <div className='order-inputs-container'>
                         תאריך איסוף:
-                        <input type='datetime-local' name='collecting_date'></input>
+                        <input type='datetime-local' name='collecting_date' ref={collecting_date_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         שם המאסף:
-                        <input type='text' name='collecting_person_name' placeholder='שם המאסף'></input>
+                        <input type='text' name='collecting_person_name' placeholder='שם המאסף' ref={collecting_person_name_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         כתובת לאיסוף:
-                        <input type='text' name='collecting_location' placeholder='כתובת לאיסוף'></input>
+                        <input type='text' name='collecting_location' placeholder='כתובת לאיסוף' ref={collecting_location_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         מס׳ טלפון לאיסוף:
-                        <input type='text' name='collecting_person_phone' placeholder='מס׳ טלפון לאיסוף'></input>
+                        <input type='text' name='collecting_person_phone' placeholder='מס׳ טלפון לאיסוף' ref={collecting_person_phone_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         סה״כ מנות:
-                        <input type='number' name='total'></input>
+                        <input type='number' name='total' ref={total_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         מתוכן מנות כשרות:
-                        <input type='number' name='total_kosher'></input>
+                        <input type='number' name='total_kosher' ref={total_kosher_ref}></input>
                     </div>
                     <div className='order-inputs-container'>
                         מתוכן צמחוניות:
-                        <input type='number' name='total_vegetarians'></input>
+                        <input type='number' name='total_vegetarians' ref={total_vegetarians_ref}></input>
                     </div>
                 </div>
                 <button className='register-btn' type='submit'>סיום</button>
